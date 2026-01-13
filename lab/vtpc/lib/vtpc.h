5f2ce3d887b7d417f981919a90573baed9a1b570
red <red05@gmail.com>
2026-01-13 23:16:57 +0300

    реализация кэша vtpc.c

--- lab/vtpc/lib/vtpc.h
+++ lab/vtpc/lib/vtpc.h
@@ -1,10 +1,19 @@
 #pragma once
 
+#include <stddef.h>
+#include <stdint.h>
 #include <sys/types.h>
 
 int vtpc_open(const char* path, int mode, int access);
 int vtpc_close(int fd);
+
 ssize_t vtpc_read(int fd, void* buf, size_t count);
 ssize_t vtpc_write(int fd, const void* buf, size_t count);
+
 off_t vtpc_lseek(int fd, off_t offset, int whence);
 int vtpc_fsync(int fd);
+
+int vtpc_advice(int fd, off_t offset, uint64_t next_use);
+
+/* stats */
+void vtpc_print_stats(int fd);
